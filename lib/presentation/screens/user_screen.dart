04678272fefecbe34bda8e525f52db230e9b164e
hrx-007 <harikrishnamanoj2@gmail.com>
2024-05-02 14:51:44 +0530

    search field added

--- lib/presentation/screens/user_screen.dart
+++ lib/presentation/screens/user_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lamie_pro/presentation/widgets/widgets.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,23 +11,23 @@ class UserScreen extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: const Text(
           "Users",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      body: SizedBox(
-        child: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.grey,
-                backgroundImage: NetworkImage(
-                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fprofile-icon-male-user-person-avatar-symbol-vector-20910837&psig=AOvVaw3XendD-y_yeSB08PahTFuh&ust=1714721846005000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCXkOy67oUDFQAAAAAdAAAAABAJ"),
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(right: 10.0, left: 10),
+          child: Column(
+            children: [
+              SearchField(),
+              Expanded(
+                child: UserListWidget(),
               ),
-              title: Text("user${index + 1}"),
-              subtitle: Text("email${index + 1}"),
-            );
-          },
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -198,3 +198,57 @@ class BackNavigationButton extends StatelessWidget {
     );
   }
 }
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      // controller: searchcontroller,
+      cursorColor: Colors.black,
+      style: const TextStyle(color: Colors.black),
+      decoration: const InputDecoration(
+          contentPadding: EdgeInsets.zero,
+          prefixIcon: Icon(Icons.search),
+          // filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          hintText: 'Search',
+          hintStyle: TextStyle(fontSize: 15, height: 1.5, color: Colors.black)),
+      onChanged: (value) {},
+    );
+  }
+}
+
+class UserListWidget extends StatelessWidget {
+  const UserListWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const CircleAvatar(
+            backgroundColor: Colors.grey,
+            backgroundImage: NetworkImage(
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vectorstock.com%2Froyalty-free-vector%2Fprofile-icon-male-user-person-avatar-symbol-vector-20910837&psig=AOvVaw3XendD-y_yeSB08PahTFuh&ust=1714721846005000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCXkOy67oUDFQAAAAAdAAAAABAJ"),
+          ),
+          title: Text("user${index + 1}"),
+          subtitle: Text("email${index + 1}"),
+        );
+      },
+    );
+  }
+}
